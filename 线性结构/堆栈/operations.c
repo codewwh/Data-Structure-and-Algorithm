dd6705631adf18db1d5f9e2c47db6e1a661cd773
codewwh <codewwh@gmail.com>
2018-01-27 22:05:49 +0800

    添加了一种双向堆栈的入栈和出栈操作

--- 线性结构/堆栈/operations.c
+++ 线性结构/堆栈/operations.c
@@ -32,3 +32,83 @@ ElementType Pop(Stack PtrS){
 		return (PtrS->Data[(PtrS->Top)--]);
 	}
 }
+
+/* -------------------------------------------------------------------------------------------------------------- */
+
+/* 用一个数组实现两个堆栈，要求最大地利用数组空间，使数组只要有空间入栈操作就可以成功。
+分析：使这两个栈分别从数组的两头开始向中间生长，当两个栈的栈顶指针相遇时，表示两个栈都满了 */
+/* 初始化 */
+#define MAXSIZE
+struct DStack{
+	ElementType Data[MAXSIZE];
+	int Top1;                   //堆栈1的栈顶指针
+	int Top2;                   //堆栈2的栈顶指针
+}S;
+
+/* 判断堆栈为空 */
+S.Top1 = -1;
+S.Top2 = MAXSIZE;
+
+/* -------------------------------------------------------------------------------------------------------------- */
+
+/* 1、入栈 */
+void DPush(struct DStack* PtrS,ElementType item,int Tag){  //Tag作为区分两个堆栈的标志，取值为1和2
+	if(PtrS->Top2 - PtrS->Top1 == 1){     //堆栈满
+	printf("堆栈满");
+	return;		
+	}
+	if(Tag == 1){
+		PtrS->Data[++(PtrS->Top1)] = item;
+	}else{
+		PtrS->Data[--(PtrS->Top2)] = item;
+	}
+}
+
+/* -------------------------------------------------------------------------------------------------------------- */
+
+/* 2、出栈 */
+void DPop(struct DStack* PtrS,int Tag){          //Tag作为区分两个堆栈的标志，取值为1和2
+	if(Tag == 1){
+		if(PtrS->Top1 == -1){                    //堆栈1空
+			printf("堆栈1空")；
+			return NULL；
+		}else{
+			return PtrS->Data[(PtrS->Top1)--];
+		}else{
+			if(PtrS->Top2 == MAXSIZE){           //堆栈2空
+				printf("堆栈2空");
+				return NULL;
+			}else{
+				return PtrS->Data[(PtrS->Top2)++];
+			}
+				
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
